d5348a18c374adbbb660e1387cbeadee7987667c
Robert Hoffmann <rob.hoffmann@posteo.de>
2023-01-06 17:46:50 +0100

    added new simple and empty extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,25 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { activateMockDebug } from './activateFshDebug';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Extension "codfsh.runSushi" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fsh-validator.runFhirFsh', () => {
+	let disposable = vscode.commands.registerCommand('codfsh.runSushi', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hallo Robert, jetzt VALIDATE_ALL!');
+		vscode.window.showInformationMessage('Running Sushi!');
 	});
 
 	context.subscriptions.push(disposable);
-
-	activateMockDebug(context);
 }
 
 // This method is called when your extension is deactivated
